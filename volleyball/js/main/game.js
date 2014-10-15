4256b4a6e1c1d5016030dd1ca74a176b3e5658b8
Mark Sliva <mark.sliva@vibes.com>
2014-10-15 09:43:33 -0500

    add player image and keyboard controls

--- volleyball/js/main/game.js
+++ volleyball/js/main/game.js
@@ -1,3 +1,4 @@
+var player1, player2, ball, refreshRate = 30;
 function start_game(Physics) {
   Physics(function(world) {
     var viewWidth = window.innerWidth
@@ -6,7 +7,6 @@ function start_game(Physics) {
       ,center = Physics.vector(viewWidth, viewHeight).mult(0.5)
     // bounds of the window
       ,viewportBounds = Physics.aabb(0, 0, viewWidth, viewHeight)
-      ,attractor
       ,edgeBounce
       ,renderer
     ;
@@ -47,60 +47,45 @@ function start_game(Physics) {
 
     }, true);
 
-    // move the attractor position to match the mouse coords
-    renderer.el.addEventListener('mousemove', function( e ){
-      attractor.position({ x: e.pageX, y: e.pageY });
-    });
+    //renderer.el.addEventListener('mousemove', function( e ){
 
-    // some fun colors
-    var colors = [
-      '#b58900',
-      '#cb4b16',
-      '#dc322f',
-      '#d33682',
-      '#6c71c4',
-      '#268bd2',
-      '#2aa198',
-      '#859900'
-    ];
     // create some bodies
     var bodies = [];
     var v = Physics.vector(0, 300);
     var circle, r;
 
-    r = 20;
+    r = 33;
     var right_x=1300;
     ball = Physics.body('circle', {
       radius: r
       ,mass: r
       ,x: right_x
       ,y: 200
-      ,vx: v.perp().mult(0.0001).x
-      ,vx: v.y
       ,styles: {
-    fillStyle: colors[ 0 ]
+    fillStyle: '#859900'
       }
     });
-    var player1 = Physics.body('rectangle', {
+    ball.view = new Image();
+    ball.view.src = 'images/volleyball.png'
+    player1 = Physics.body('rectangle', {
       // place the centroid of the rectangle at (300, 200)
       x: 300,
       y: 200,
-      width: 30,
-      height: 90
+      width: 400,
+      height: 300
     });
-    var player2 = Physics.body('rectangle', {
+    player1.view = new Image();
+    player1.view.src = 'images/p1.png';
+    player2 = Physics.body('rectangle', {
       // place the centroid of the rectangle at (300, 200)
       x: (right_x + 20),
       y: 200,
       width: 30,
-      height: 90
+      height: 300
     });
     bodies.push(ball);
     bodies.push(player1);
     bodies.push(player2);
-    v.perp(true)
-    .mult(10000)
-    .rotate(1 / 3);
 
     // add things to the world
     world.add( bodies );
@@ -111,7 +96,6 @@ function start_game(Physics) {
     })
       ,Physics.behavior('body-impulse-response')
       ,edgeBounce
-      ,attractor
     ]);
 
     // subscribe to ticker to advance the simulation
@@ -123,50 +107,87 @@ function start_game(Physics) {
     Physics.util.ticker.start();
   });
 }
-//function keyMove() {
-//  keys.forEach(function(value, key) {
-//
-//    if(value == true) {
-//      switch (key) {
-//        case 65:
-//          // a key (left)
-//
-//          moveLeft(p2);
-//        break;
-//
-//        case 68:
-//          // d key (right)
-//
-//          moveRight(p2);
-//        break;
-//
-//        case 87:
-//          // w key (up)
-//
-//          //jump(p2);
-//        break;
-//
-//        default:
-//          break;
-//      }
-//    }
-//  }
-//}
-//
-//var keys = [];
-//gameLoop();
-//
-//function gameLoop(){
-//  setTimeout(function(){
-//    keyMove();
-//    gameLoop();
-//  },1000/refreshRate);
-//}
-//
-//function moveRight(noun) {
-//  noun.x += 50;
-//}
-//
-//function moveLeft(noun) {
-//  noun.x -= 50;
-//}
+
+function keyMove() {
+  keys.forEach(function(value, key) {
+
+    if(value == true) {
+      switch (key) {
+        case 38:
+          // up key
+          jump(player1);
+        break;
+
+        case 37:
+        case 97:
+          // left key
+
+          moveLeft(player1);
+          break;
+
+        case 39:
+        case 100:
+          // right key
+
+          moveRight(player1);
+          break;
+
+        case 65:
+          // a key (left)
+
+          moveLeft(player2);
+        break;
+
+        case 68:
+          // d key (right)
+
+          moveRight(player2);
+        break;
+
+        case 87:
+          // w key (up)
+
+          jump(player2);
+        break;
+
+        default:
+          break;
+      }
+    }
+  });
+}
+
+var moveSpeed = .003;
+var keys = [];
+
+function gameLoop(){
+  setTimeout(function(){
+    keyMove();
+    gameLoop();
+  },1000/refreshRate);
+}
+
+function moveRight(noun) {
+  var x_position = noun.state.pos.x;
+  if(x_position < 350) {
+    noun.state.acc.set(moveSpeed,null);
+  }
+}
+
+function moveLeft(noun) {
+  noun.state.acc.set((-1 * moveSpeed),null);
+}
+
+function jump(noun) {
+  noun.state.acc.set(null,-1*(moveSpeed));
+}
+
+function keyChangeHandler(e){
+  keys[e.keyCode] = e.type == 'keydown';
+  e.preventDefault();
+}
+
+// Listen for when the user presses a key down or up
+window.addEventListener("keydown", keyChangeHandler, true);
+window.addEventListener("keyup", keyChangeHandler, true);
+gameLoop();
